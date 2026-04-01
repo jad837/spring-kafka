bb0a31a2322d098db4f7c63e3db4f2c3d545ef1b
jad837 <jadhavsaurabh037@gmail.com>
2026-04-01 13:58:40 +0000

    Remove unused imports
    Signed-off-by: jad837 <jadhavsaurabh037@gmail.com>

--- spring-kafka/src/test/java/org/springframework/kafka/config/StreamsBuilderFactoryBeanTests.java
+++ spring-kafka/src/test/java/org/springframework/kafka/config/StreamsBuilderFactoryBeanTests.java
@@ -21,7 +21,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 import java.util.regex.Pattern;
 
